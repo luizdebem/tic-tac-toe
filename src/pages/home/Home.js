e5cda3388f37fd612430b8d64e82daf6df05f6b8
luizdebem <luizdebem99@gmail.com>
2022-02-16 22:22:27 -0300

    Cell look up instead of O(i * j) on updateValue

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,9 +3,9 @@ import './Home.css';
 
 function App() {
   const [game, setGame] = useState([
-    [{ state: "", id: 0 }, { state: "", id: 1 }, { state: "", id: 2 }],
-    [{ state: "", id: 3 }, { state: "", id: 4 }, { state: "", id: 5 }],
-    [{ state: "", id: 6 }, { state: "", id: 7 }, { state: "", id: 8 }]
+    [{ state: "", i: 0, j: 0 }, { state: "", i: 0, j: 1 }, { state: "", i: 0, j: 2 }],
+    [{ state: "", i: 1, j: 0 }, { state: "", i: 1, j: 1 }, { state: "", i: 1, j: 2 }],
+    [{ state: "", i: 2, j: 0 }, { state: "", i: 2, j: 1 }, { state: "", i: 2, j: 2 }]
   ]);
 
   // "X" || "O"
@@ -15,37 +15,32 @@ function App() {
     turn === "X" ? setTurn("O") : setTurn("X");
   };
 
-  const updateValue = (cellID) => {
-    for (let i = 0; i < game.length; i++) {
-      for (let j = 0; j < game[i].length; j++) {
-        if (game[i][j].id === cellID && game[i][j].state === "") {
-          game[i][j].state = turn;
-          switchTurn();
-          setGame(game);
-          break;
-        }
-      }
+  const updateValue = (i, j) => {
+    if (game[i][j].state === "") {
+      game[i][j].state = turn;
+      switchTurn();
+      setGame(game);
     };
   }
 
-  const handleCellClick = (cellID) => {
-    updateValue(cellID);
+  const handleCellClick = (i, j) => {
+    updateValue(i, j);
   };
 
-  const renderedGame = () => {
+  const renderGame = () => {
     const response = [];
     for (let i = 0; i < game.length; i++) {
       for (let j = 0; j < game[i].length; j++) {
-        response.push(<div className="cell" key={game[i][j].id} onClick={() => { handleCellClick(game[i][j].id); }} > {game[i][j].state} </div>);
+        response.push(<div className="cell" onClick={() => { handleCellClick(i, j); }}> {game[i][j].state} </div>);
       }
-    }
+    };
     return response;
   };
 
   return (
     <div className="page-container">
       <div className="game-container">
-        {renderedGame()}
+        {renderGame()}
       </div>
     </div>
   );
