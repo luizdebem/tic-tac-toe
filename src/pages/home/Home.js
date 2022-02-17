2faadb3faa5ab9099b10c48dd479273ffbb7a974
luizdebem <luizdebem99@gmail.com>
2022-02-16 21:32:11 -0300

    Layout OK

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,7 +2,13 @@ import './Home.css';
 
 function App() {
   return (
-    <div>Asd</div>
+    <div className="page-container">
+      <div className="game-container">
+        <div className="cell">X</div> <div className="cell">O</div> <div className="cell winner">O</div>
+        <div className="cell">X</div> <div className="cell">X</div> <div className="cell winner">O</div>
+        <div className="cell">O</div> <div className="cell">X</div> <div className="cell winner">O</div>
+      </div>
+    </div>
   );
 }
 
